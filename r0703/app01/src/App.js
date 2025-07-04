b9fdd3d33f22d57eba1e5be064d71ee880e15915
minkim-korea <km24778@gmail.com>
2025-07-04 15:27:04 +0900

    0704

--- r0703/app01/src/App.js
+++ r0703/app01/src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-// const btn1 = () => {
-//   alert("경고창");
-// }   
-const btn1 = function(){
-  alert("안녕하세요");
-}
-//  let btn1 = function(){
-//    alert("경고창");
-//   }
+   const btn = () => {
+    alert("로그인 진행합니다.");
+   }
 
 
   return (
     <div className="App">
-
-          <h2>안녕하세요.</h2>
-          <button onClick={btn1}>버튼</button>
-
+   
+          <h2>로그인</h2>      
+    <form>
+        <label>아이디</label>
+        <input type="text"/><br/>
+        <label>패스워드</label>
+        <input type="test"/><br/><br/>
+    </form>                  
+      <button onClick={btn}>로그인</button>
     </div>
   );
 }
